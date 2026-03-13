28f7ec570f3b1d414fcc6c4f792717041f65a4b8
Quentin <quentin.drigeard@edu.ece.fr>
2026-03-13 10:10:48 +0100

    Employe attribut/constru/getter/setter/méthode

--- src/Employe.java
+++ src/Employe.java
@@ -1,4 +1,39 @@
 public class Employe {
+
+    //Attributs
     private int id;
+    private String nom;
+    private String prenom;
+    private String poste;
+    private String email;
+
+    //Constructeur
+    public Employe (int id ,String nom, String prenom, String poste, String email){
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.poste = poste;
+        this.email = email;
+
+    }
+
+    //getter
+    public int getId(){return this.id;}
+    public String getnom(){return this.nom;}
+    public String getPrenom(){return this.prenom;}
+    public String getPoste(){return this.poste;}
+    public String getEmail(){return this.email;}
+
+    //setter
+    public void setNom(String newNom){this.nom = newNom;}
+    public void setPrenom(String newPrenom){this.prenom = newPrenom;}
+    public void setPoste(String newPoste){this.poste = newPoste;}
+    public void setEmail(String newEmail){this.email = newEmail;}
+
+    //métohde
+    public void afficherDetails(){
+
+    }
+
 
 }
